0f2382817a0c4e20752c92a0f42988fb8abfcfb3
Petr Ilin <hevav@hevav.dev>
2022-01-11 19:11:47 +0300

    Overlay the Registry with the PLAY one

---
+++ plugin/src/main/java/net/elytrium/limboapi/utils/OverlayIntMap.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2021 Elytrium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.elytrium.limboapi.utils;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Streams;
+import io.netty.util.collection.IntObjectMap;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
+
+public class OverlayIntMap<K> extends OverlayMap<Integer, K> implements IntObjectMap<K> {
+  public OverlayIntMap(Map<Integer, K> parent, Map<Integer, K> overlay) {
+    super(parent, overlay);
+  }
+
+  @Override
+  public K get(int key) {
+    return super.get(key);
+  }
+
+  @Override
+  public K put(int key, K value) {
+    return super.put(key, value);
+  }
+
+  @Override
+  public K remove(int key) {
+    return super.remove(key);
+  }
+
+  @Override
+  public Iterable<PrimitiveEntry<K>> entries() {
+    return Iterables.concat(((IntObjectMap<K>) parent).entries(), ((IntObjectMap<K>) overlay).entries());
+  }
+
+  @Override
+  public boolean containsKey(int key) {
+    return super.containsKey(key);
+  }
+
+  @Override
+  public Set<Integer> keySet() {
+    return Streams.concat(this.parent.keySet().stream(), this.overlay.keySet().stream()).collect(Collectors.toSet());
+  }
+
+  @NotNull
+  @Override
+  public Collection<K> values() {
+    return Streams.concat(this.parent.values().stream(), this.overlay.values().stream()).collect(Collectors.toList());
+  }
+
+  @NotNull
+  @Override
+  public Set<Entry<Integer, K>> entrySet() {
+    return Streams.concat(this.parent.entrySet().stream(), this.overlay.entrySet().stream()).collect(Collectors.toSet());
+  }
+}
---
+++ plugin/src/main/java/net/elytrium/limboapi/utils/SetIsObjectSet.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2021 Elytrium
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.elytrium.limboapi.utils;
+
+import it.unimi.dsi.fastutil.objects.ObjectIterator;
+import it.unimi.dsi.fastutil.objects.ObjectSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+import org.jetbrains.annotations.NotNull;
+
+public class SetIsObjectSet<K> implements ObjectSet<K> {
+  private final Set<K> set;
+
+  public SetIsObjectSet(Set<K> set) {
+    this.set = set;
+  }
+
+  @Override
+  public int size() {
+    return this.set.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return this.set.isEmpty();
+  }
+
+  @Override
+  public boolean contains(Object o) {
+    return this.set.contains(o);
+  }
+
+  @Override
+  public ObjectIterator<K> iterator() {
+    return new IteratorIsObjectIterator<>(this.set.iterator());
+  }
+
+  @NotNull
+  @Override
+  public Object[] toArray() {
+    return this.set.toArray();
+  }
+
+  @NotNull
+  @Override
+  public <T> T[] toArray(@NotNull T[] ts) {
+    return this.set.toArray(ts);
+  }
+
+  @Override
+  public boolean add(K k) {
+    return this.set.add(k);
+  }
+
+  @Override
+  public boolean remove(Object o) {
+    return this.set.remove(o);
+  }
+
+  @Override
+  public boolean containsAll(@NotNull Collection<?> collection) {
+    return this.set.containsAll(collection);
+  }
+
+  @Override
+  public boolean addAll(@NotNull Collection<? extends K> collection) {
+    return this.set.addAll(collection);
+  }
+
+  @Override
+  public boolean removeAll(@NotNull Collection<?> collection) {
+    return this.set.removeAll(collection);
+  }
+
+  @Override
+  public boolean retainAll(@NotNull Collection<?> collection) {
+    return this.set.retainAll(collection);
+  }
+
+  @Override
+  public void clear() {
+    this.set.clear();
+  }
+
+  private static final class IteratorIsObjectIterator<K> implements ObjectIterator<K> {
+
+    private final Iterator<K> iterator;
+
+    private IteratorIsObjectIterator(Iterator<K> iterator) {
+      this.iterator = iterator;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return this.iterator.hasNext();
+    }
+
+    @Override
+    public K next() {
+      return this.iterator.next();
+    }
+  }
+}
