c35fa061ba2f8cae7dc2dfcd632318fed2f2ce78
UserNugget <47762903+UserNugget@users.noreply.github.com>
2023-10-29 14:42:12 +0300

    Ensure that the packets after a `LoginAcknowledged` is not handled by the `LoginTrackHandler`

--- plugin/src/main/java/net/elytrium/limboapi/injection/login/LoginTrackHandler.java
+++ plugin/src/main/java/net/elytrium/limboapi/injection/login/LoginTrackHandler.java
@@ -19,13 +19,20 @@ package net.elytrium.limboapi.injection.login;
 
 import com.velocitypowered.proxy.connection.MinecraftConnection;
 import com.velocitypowered.proxy.connection.MinecraftSessionHandler;
+import com.velocitypowered.proxy.protocol.MinecraftPacket;
 import com.velocitypowered.proxy.protocol.StateRegistry;
 import com.velocitypowered.proxy.protocol.packet.LoginAcknowledged;
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.internal.PlatformDependent;
+import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
+import net.elytrium.limboapi.LimboAPI;
 
 public class LoginTrackHandler implements MinecraftSessionHandler {
   private final CompletableFuture<Object> confirmation = new CompletableFuture<>();
+  private final Queue<MinecraftPacket> queuedPackets = PlatformDependent.newMpscQueue();
   private final MinecraftConnection connection;
 
   public LoginTrackHandler(MinecraftConnection connection) {
@@ -33,7 +40,14 @@ public class LoginTrackHandler implements MinecraftSessionHandler {
   }
 
   public void waitForConfirmation(Runnable runnable) {
-    this.confirmation.thenRun(runnable);
+    this.confirmation.thenRun(runnable).thenRun(this::processQueued);
+  }
+
+  @Override
+  public void handleGeneric(MinecraftPacket packet) {
+    if (this.connection.getState() == StateRegistry.CONFIG) {
+      this.queuedPackets.add(packet);
+    }
   }
 
   @Override
@@ -46,5 +60,31 @@ public class LoginTrackHandler implements MinecraftSessionHandler {
   @Override
   public void handleUnknown(ByteBuf buf) {
     this.connection.close(true);
+  }
+
+  public void processQueued() {
+    ChannelHandlerContext ctx = this.connection.getChannel().pipeline().context(this.connection);
+    MinecraftSessionHandler sessionHandler = this.connection.getActiveSessionHandler();
+    if (sessionHandler != null && !sessionHandler.beforeHandle()) {
+      for (MinecraftPacket packet : this.queuedPackets) {
+        if (!this.connection.isClosed()) {
+          try {
+            if (!packet.handle(sessionHandler)) {
+              sessionHandler.handleGeneric(packet);
+            }
+          } catch (Throwable throwable) {
+            try {
+              this.connection.exceptionCaught(ctx, throwable);
+            } catch (Throwable t) {
+              LimboAPI.getLogger().error("{}: exception handling exception in {}", ctx.channel().remoteAddress(), this, t);
+            }
+          }
+        }
+
+        ReferenceCountUtil.release(packet);
+      }
+    }
+
+    this.queuedPackets.clear();
   }
 }
