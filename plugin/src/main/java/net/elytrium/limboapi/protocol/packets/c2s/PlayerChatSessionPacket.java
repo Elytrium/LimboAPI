8ff8d8e336eac7ab8c3c45f5dddcfca45ddef59c
UserNugget <usernugget@eternlix.net>
2024-01-27 12:54:06 +0300

    Support for 1.19.3+ chat signing
    1) This packet is registered only for 1.19.3+
    2) 1.19.3+ no longer send public key during login
    3) 1.19.3+ send random uuid as holder
    4) 1.19.3+ itself does not send ChatSession if its uuid is changed, so this hook is useless
    
    Fixes https://github.com/Elytrium/LimboAPI/issues/87

--- plugin/src/main/java/net/elytrium/limboapi/protocol/packets/c2s/PlayerChatSessionPacket.java
+++ plugin/src/main/java/net/elytrium/limboapi/protocol/packets/c2s/PlayerChatSessionPacket.java
@@ -52,16 +52,6 @@ public class PlayerChatSessionPacket implements MinecraftPacket {
 
   @Override
   public boolean handle(MinecraftSessionHandler minecraftSessionHandler) {
-    // LimboAPI hook - discard if there is no identified key or unmatched UUID
-    if (minecraftSessionHandler instanceof ClientPlaySessionHandler playSessionHandler) {
-      try {
-        ConnectedPlayer player = (ConnectedPlayer) PLAYER_FIELD.invokeExact(playSessionHandler);
-        return player.getIdentifiedKey() == null || player.getUniqueId() != this.holderId;
-      } catch (Throwable e) {
-        throw new ReflectionException(e);
-      }
-    }
-
     return false;
   }
 
