74424b462c2a8b793bc43d5e669bf3ab6fb844b1
Roman Makeev <57789105+makeevrserg@users.noreply.github.com>
2026-03-28 12:47:55 +0300

    Improve gradle (#234)

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            name = "elytrium-repo"
+            url = uri("https://maven.elytrium.net/repo/")
+        }
+        maven {
+            name = "papermc-repo"
+            url = uri("https://repo.papermc.io/repository/maven-public/")
+        }
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "limboapi"
+
+include("api")
+include("plugin")
